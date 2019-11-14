cf2649f1b394549a52b3ca0cc362c30da5285552
Jack Fletcher <jdfletch97@gmail.com>
2019-11-14 17:56:37 -0600

    Change style of LineChart cursor

--- src/components/LineChart/index.js
+++ src/components/LineChart/index.js
@@ -43,23 +43,36 @@ const Cursor = ({x, y, datum, totalShots}) => {
   let xLoc;
   let newTextAnchor;
 
-  if (datum.x < 25) {
-    xLoc = x + 5;
+  if (datum.x < 30) {
+    xLoc = x + 10;
     newTextAnchor = 'start';
   } else {
-    xLoc = x - 5;
+    xLoc = x - 10;
     newTextAnchor = 'end';
   }
   return (
     <g>
       <text
         x={xLoc}
-        y={60}
-        style={{textAnchor: newTextAnchor, fontSize: '14px'}}
+        y={30}
+        style={{textAnchor: newTextAnchor, fontSize: '16px'}}
       >{`${((100 * datum.y) / totalShots).toFixed(2)}%`}</text>
+      <text
+        x={xLoc}
+        y={50}
+        style={{textAnchor: newTextAnchor, fontSize: '14px'}}
+      >{`${datum.y} shot${datum.y === 1 ? '' : 's'}`}</text>
+      <text
+        x={xLoc}
+        y={65}
+        style={{textAnchor: newTextAnchor, fontSize: '14px'}}
+      >{`@ ${datum.x} ft`}</text>
       <path
-        d={`M${x},250 L${x},50`}
-        style={{strokeWidth: 1, stroke: 'rgba(0, 0, 0, 0.2)'}}
+        d={`M${x},250 L${x},20`}
+        style={{
+          strokeWidth: 10,
+          stroke: 'rgba(0, 0, 0, 0.2)',
+        }}
       />
     </g>
   );
