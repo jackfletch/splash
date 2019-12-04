cdb86673cba970579291f70f6cf19b9fd2606a5d
Jack Fletcher <jdfletch97@gmail.com>
2019-12-03 18:29:20 -0600

    Refactor PlayerSelector to a functional component

--- src/components/PlayerSelector/index.js
+++ src/components/PlayerSelector/index.js
@@ -1,40 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class PlayerSelector extends React.Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    const {player} = this.props;
-    if (nextProps.player !== player) {
-      return true;
-    }
-    return false;
-  }
-
-  render() {
-    const {player, players, setPlayerId} = this.props;
-    return (
-      <form>
-        <label htmlFor="select1">
-          Select
-          <select
-            value={player}
-            onChange={e => setPlayerId(parseInt(e.target.value))}
-          >
-            {players.map(d => (
-              <option key={d.id} value={d.id}>
-                {d.name}
-              </option>
-            ))}
-          </select>
-        </label>
-      </form>
-    );
-  }
-}
+const PlayerSelector = ({player, players, setPlayerId}) => (
+  <form>
+    <label htmlFor="select1">
+      Select
+      <select
+        value={player}
+        onChange={e => setPlayerId(parseInt(e.target.value))}
+      >
+        {players.map(d => (
+          <option key={d.id} value={d.id}>
+            {d.name}
+          </option>
+        ))}
+      </select>
+    </label>
+  </form>
+);
 
 PlayerSelector.propTypes = {
   player: PropTypes.number.isRequired,
-  players: PropTypes.array.isRequired,
+  players: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ).isRequired,
   setPlayerId: PropTypes.func.isRequired,
 };
 
