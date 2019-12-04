5f8923517f6853fc5d18e7bd0f4d1b5e959483cb
Jack Fletcher <jdfletch97@gmail.com>
2019-12-03 20:28:54 -0600

    Refactor Charts content from ChartDashboard

---
+++ src/containers/Charts/index.js
@@ -0,0 +1,67 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import media from '../../components/style-utils';
+
+import HexShotchart from '../../components/HexShotchart';
+import VShootingSignature from '../../components/VShootingSignature';
+import BarChart from '../../components/BarChart';
+import {useShotsApi} from '../../hooks';
+
+const ChartsDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  ${media.tablet`flex-direction: column;`}
+`;
+
+const Charts = ({playerId}) => {
+  const maxDistance = 35;
+  const [activated, setActivated] = useState(0);
+  const [deactivated, setDeactivated] = useState(0);
+  const [{data, ribbonedData, binnedData}] = useShotsApi(playerId, maxDistance);
+
+  if (
+    data === undefined ||
+    ribbonedData.length === 0 ||
+    Object.keys(binnedData).length === 0
+  ) {
+    return <div>Loading player data</div>;
+  }
+  if (data.length === 0) {
+    return <div>No result found for this player</div>;
+  }
+  if (data.length <= 50) {
+    return <div>Not enough shots by this player for any meaningful data</div>;
+  }
+
+  const hover = {
+    distance: activated,
+    toggle: activated !== deactivated,
+  };
+
+  return (
+    <ChartsDiv>
+      <HexShotchart data={data} hover={hover} />
+      <VShootingSignature
+        data={ribbonedData}
+        hover={hover}
+        maxDistance={maxDistance}
+      />
+      <BarChart
+        data={binnedData}
+        hover={hover}
+        maxDistance={maxDistance}
+        setActivated={setActivated}
+        setDeactivated={setDeactivated}
+      />
+    </ChartsDiv>
+  );
+};
+
+Charts.propTypes = {
+  playerId: PropTypes.number,
+};
+
+export default Charts;
