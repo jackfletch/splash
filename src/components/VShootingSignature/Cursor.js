e1937b93933113ac57f257baf1388a60680feed0
Jack Fletcher <jdfletch97@gmail.com>
2019-12-29 17:46:32 -0600

    Refactor ShootingSignature cursor y limits

--- src/components/VShootingSignature/Cursor.js
+++ src/components/VShootingSignature/Cursor.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Cursor = ({scale, x}) => (
-  <g>
-    <path
-      d={`M${scale.x(x + 0.5)},200 L${scale.x(x + 0.5)},20`}
-      style={{strokeWidth: 10, stroke: 'rgba(0, 0, 0, 0.2)'}}
-    />
-  </g>
-);
+const Cursor = ({scale, x}) => {
+  const top = scale.y(1);
+  const bottom = scale.y(0);
+  return (
+    <g>
+      <path
+        d={`M${scale.x(x + 0.5)},${top} L${scale.x(x + 0.5)},${bottom}`}
+        style={{strokeWidth: 10, stroke: 'rgba(0, 0, 0, 0.2)'}}
+      />
+    </g>
+  );
+};
 
 Cursor.propTypes = {
   scale: PropTypes.shape({
