62da7068091dc3f1fc45e27312ec179e0e8832a3
Jack Fletcher <jdfletch97@gmail.com>
2020-02-29 14:13:37 -0600

    Add noninteractive wrapped SVG component
    This component prevents scrolling on mobile if trying to swipe on the
    SVG. Counterintuitively, this creates a better interaction experience by
    keeping the screen in place when hovering a chart.

--- src/components/LeftRightChart/style.js
+++ src/components/LeftRightChart/style.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import styled from 'styled-components';
 
 export const Svg = styled.svg`
@@ -5,8 +6,21 @@ export const Svg = styled.svg`
   margin: 0 auto;
   height: auto;
   width: 100%;
+  pointer-events: all;
 `;
 
 export const Rect = styled.rect`
   fill: rgba(43, 49, 55, 0.5);
 `;
+
+const NoninteractiveDiv = styled.div`
+  user-select: none;
+  pointer-events: none;
+  touch-action: none;
+`;
+
+const withoutInteraction = component => (
+  <NoninteractiveDiv>{component}</NoninteractiveDiv>
+);
+
+export const StaticSvg = withoutInteraction(Svg);
